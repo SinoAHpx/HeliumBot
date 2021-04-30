ad81fd2559f7c6a2f92a59da08a5b6c4da7b1f99
AHpx <agpx@yandex.com>
2021-04-30 12:55:19 +0800

    Build up

---
+++ Plugins/Group/HelloWorldPlugin.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using Mirai_CSharp;
+using Mirai_CSharp.Models;
+using Mirai_CSharp.Plugin.Interfaces;
+
+namespace HeliumBot.Plugins.Group
+{
+    public class HelloWorldPlugin : IGroupMessage
+    {
+        public async Task<bool> GroupMessage(MiraiHttpSession session, IGroupMessageEventArgs e)
+        {
+            if (e.Sender.Group.Id == 110838222)
+            {
+                if (e.Chain[1].ToString() == "-hello")
+                {
+                    await session.ExecuteCommandAsync("hello");
+                }
+                
+            }
+            
+            return false;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using HeliumBot.Plugins.Group;
+using Mirai_CSharp;
+using Mirai_CSharp.Models;
+
+namespace HeliumBot
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var sessionOptions = new MiraiHttpSessionOptions("127.0.0.1",29331,"5a72244029a548e88c262ae31750eb52");
+            var session = new MiraiHttpSession();
+            
+            session.AddPlugin(new HelloWorldPlugin());
+
+            await session.ConnectAsync(sessionOptions, 2672886221);
+
+            await session.RegisterCommandAsync("hello", null, null, "-");
+
+            session.CommandExecuted += async (sender, eventArgs) =>
+            {
+                await session.SendGroupMessageAsync(eventArgs.Group,
+                    new PlainMessage($"Hi {eventArgs.Sender}! I'm a helium bot"));
+                
+                return false;
+            };
+            
+            while (true)
+            {
+                if (Console.ReadLine() == "exit")
+                {
+                    return;
+                }
+            }
+        }
+    }
+}
