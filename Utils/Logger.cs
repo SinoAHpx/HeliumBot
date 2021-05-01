a4338fcac8cb6f0727f332cecc95d364b4bb8db1
AHpx <agpx@yandex.com>
2021-05-01 14:27:05 +0800

    Added Logger.cs

---
+++ Utils/Logger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HeliumBot.Utils
+{
+    public static class Logger
+    {
+        public static void Log(string text)
+        {
+            Console.WriteLine($"[{DateTime.Now:s}] {text}");
+        }
+
+        public static void Log(params string[] text)
+        {
+            Console.WriteLine($"[{DateTime.Now:s}] {string.Join(" ", text)}");
+        }
+    }
+}
