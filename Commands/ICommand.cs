38ec6568df61eac97470835ab280cb6f07520e40
AHpx <agpx@yandex.com>
2021-05-03 12:55:21 +0800

    Added ICommand.cs

---
+++ Commands/ICommand.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace HeliumBot.Commands
+{
+    public interface ICommand
+    {
+        //demos:
+        // /genshin 114514 -avatar
+        // /genshin abyss 114514
+        public Task<string[]> Execute(string[] command);
+    }
+}
